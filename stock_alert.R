c8727da0ac77f7a162180a378d15f684e775342f
Sanjaya J Shetty <61221236+SanjayShetty01@users.noreply.github.com>
2026-03-12 14:39:38 +0530

    Change SMTP port from 587 to 465

--- stock_alert.R
+++ stock_alert.R
@@ -12,7 +12,7 @@ EMAIL_PASS <- Sys.getenv("EMAIL_PASS")
 send_email <- function(subject, body, to) {
   smtp <- emayili::server(
     host     = "smtp.gmail.com",
-    port     = 587,
+    port     = 465,
     username = EMAIL_USER,
     password = EMAIL_PASS
   )
